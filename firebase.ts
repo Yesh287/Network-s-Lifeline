94d74fe3661fc2c3d4c84c5b700c5c2a8bacc031
Yesh Agarwal <138776598+Yesh287@users.noreply.github.com>
2025-10-23 01:47:39 +0530

    Remove sensitive Firebase configuration details
    Cleared Firebase configuration values for security.

--- firebase.ts
+++ firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCFxEBVBIVj_XNrkwz3AUkNAG_wXh7kPak",
-  authDomain: "netview-ee1a3.firebaseapp.com",
-  projectId: "netview-ee1a3",
-  storageBucket: "netview-ee1a3.firebasestorage.app",
-  messagingSenderId: "765547639199",
-  appId: "1:765547639199:web:6ba3286e12795794aa8daa"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
